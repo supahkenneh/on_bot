da5861c960c004bb281439d547dabbc51e98acb6
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-11 13:29:21 -1000

    discord bot will respond to command ?livenow

--- app.js
+++ app.js
@@ -3,34 +3,21 @@ const Discord = require('discord.js');
 const Config = require('./config.json');
 const Token = process.env.DISCORD_TOKEN;
 
-// console.log(Token);
-// console.log(Config);
-// console.log(process.env.DISCORD_CLIENT_ID);
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on('ready', async () => {
+
+bot.on('ready', () => {
+  console.log(bot);
+  // message.reply(`${bot.user.tag} online!`)
   console.log(`Logged in as ${bot.user.tag}! It's running on ${bot.guilds.size} servers!`)
-  bot.user.setActivity('sup', {type: 'LISTENING'});
+  // bot.user.setActivity('sup', {type: 'LISTENING'});
 });
 
-bot.on('message', async message => {
-  if (message.content === "ping") {
-    message.reply("pong");
+bot.on('message', message => {
+  if (message.content === "?livenow") {
+    //on command, will notify that user is streaming
+    message.channel.send("*twitch user* is streaming now!");
   }
 });
 
 bot.login(Token);
-
-// console.log(client);
-
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
-
-// client.on('message', msg => {
-//   if (msg === 'ping') {
-//     msg.reply('Pong!')
-//   }
-// });
-
-// client.login('token');
