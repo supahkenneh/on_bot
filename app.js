c08449996afd0d0d5e3cd875c8dc3f36324ba1b4
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-11 10:57:18 -1000

    bot is logged on

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const Config = require('./config.json');
+const Token = process.env.DISCORD_TOKEN;
+
+// console.log(Token);
+// console.log(Config);
+// console.log(process.env.DISCORD_CLIENT_ID);
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on('ready', async () => {
+  console.log(`Logged in as ${bot.user.tag}! It's running on ${bot.guilds.size} servers!`)
+  bot.user.setActivity('sup', {type: 'STREAMING'});
+});
+
+bot.on('message', async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  message.channel.send('This is a test!');
+});
+
+bot.login(Token);
+
+// console.log(client);
+
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
+
+// client.on('message', msg => {
+//   if (msg === 'ping') {
+//     msg.reply('Pong!')
+//   }
+// });
+
+// client.login('token');
