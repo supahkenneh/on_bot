32517b6ce11a7f08d3233675a86a45c30252582c
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-11 14:10:30 -1000

    test with twitch

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const Token = process.env.DISCORD_TOKEN;
 
 const bot = new Discord.Client({disableEveryone: true});
 
-
 bot.on('ready', () => {
-  console.log(bot);
+  // console.log(bot);
   // message.reply(`${bot.user.tag} online!`)
   console.log(`Logged in as ${bot.user.tag}! It's running on ${bot.guilds.size} servers!`)
   // bot.user.setActivity('sup', {type: 'LISTENING'});
 });
 
 bot.on('message', message => {
-  if (message.content === "?livenow") {
+  if (message.content.substring(0, 8) === '?livenow') {
+    let user = message.content.substring(9);
     //on command, will notify that user is streaming
-    message.channel.send("*twitch user* is streaming now!");
+    message.channel.send(`showing live followers for ${user}`);
   }
 });
 
