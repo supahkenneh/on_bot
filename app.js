34f36872d1c36d3c8e0b6d4bb99164a4196ae606
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-08-11 18:58:40 -1000

    we deed it

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const Config = require('./config.json');
 const Token = process.env.DISCORD_TOKEN;
+const findFollows = require('./index')
+
 
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on('ready', () => {
-  // console.log(bot);
-  // message.reply(`${bot.user.tag} online!`)
   console.log(`Logged in as ${bot.user.tag}! It's running on ${bot.guilds.size} servers!`)
-  // bot.user.setActivity('sup', {type: 'LISTENING'});
 });
 
 bot.on('message', message => {
-  if (message.content.substring(0, 8) === '?livenow') {
-    let user = message.content.substring(9);
-    //on command, will notify that user is streaming
-    message.channel.send(`showing live followers for ${user}`);
+  if (message.content.charAt(0) === '?') {
+    let user = message.content.substring(1);
+    console.log(user);
+    return findFollows.generateUserID(message, user)
+  } else if(message.content === "/getjaa") {
+    return message.channel.send("Hotfiyah");
   }
 });
 
--- index.js
+++ index.js
@@ -1,66 +1,64 @@
 var api = require('twitch-api-v5');
- 
-api.clientID = 'SECRET';
+var request = require("request");
+require("dotenv").config();
 
-// api.users.userByID({ userID: '57757860' }, (err, res) => {
-//     if(err) {
-//         console.log(err);
-//     } else {
-//         console.log(res);
-//         /* Example response
-//         {
-//             display_name: 'Twitch',
-//             _id: '12826',
-//             name: 'twitch',
-//             type: 'user',
-//             ...
-//         }
-//         */
-//     }
-// });
+function generateUserID(msg, username) {
+    request.get({
+            url: `https://api.twitch.tv/helix/users?login=${username}`,
+            headers: {
+                'Client-ID': process.env.TWITCH_CLIENT_ID,
+                'Accept': 'application/vnd.twitchtv.v5+json',
+            }
+        },
+        (req, res) => {
+            userId = JSON.parse(res.toJSON().body).data[0].id;
+            request.get({
+                    type: 'GET',
 
-/////////////////////////////////////////////////////
-
-api.users.follows({userID: '57757860'}, (err, res) => {
-    if (err) {
-        console.log(err);
-    } else {
-        for (var i=0; i<res.follows.length; i++) {
-            // console.log("loop");
-            // if (res.follows[i].channel.name === "riotgames") {
-            //     console.log(res.follows[i]);
-            // }
-
-            api.streams.live({channel: res.follows[i].channel._id}, (err, res) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    if (res._total > 0) {
-                        console.log(res);
-                        console.log("*********");
-                        console.log(res.streams[0].stream_type)
-                        console.log("*********");
+                    url: `https://api.twitch.tv/kraken/users/${userId}/follows/channels`,
+                    headers: {
+                        'Client-ID': process.env.TWITCH_CLIENT_ID,
+                        Accept: 'application/vnd.twitchtv.v5+json'
                     }
+                },
+                (req, res) => {
+                    let json = JSON.parse(res.body);
+                    console.log(json.follows);
+                    return new Promise(resolve => {
+                        resolve(
+                            json.follows.map(channel => {
+                                return channel.channel._id;
+                            })
+                        );
+                    }).then(result => {
+                        const queryStr = result.join(',');
+                        request.get({
+                                type: 'GET',
+
+                                url: `https://api.twitch.tv/kraken/streams/?channel=${queryStr}&stream_type=live`,
+                                headers: {
+                                    'Client-ID': 'd4t8xvlad2p46ju6nmr8bxb1qd62un',
+                                    Accept: 'application/vnd.twitchtv.v5+json'
+                                }
+                            },
+                            (req, res) => {
+                                let json2 = JSON.parse(res.body);
+                                json2.streams.map(element => {
+                                    return msg.channel.send([ 
+                                        element.channel.display_name,
+                                        element.channel.url
+                                    ]);
+                                });
+                            }
+                        );
+                    });
                 }
-            })
+            );
+        },
+    )
+}
 
-        }
-        // console.log(res.follows[0]);
-    }
-})
- 
-/////////////////////////////////////////////////////
-// '36029255'
-// api.streams.live({channel: '26301881'}, (err, res) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(res);
-//     }
-// })
 
-/////////////////////////////////////////////////////
-
-// api.feed.createPost({ auth: 'OAuth ...', channelID: '12826', post: 'New Post!' }, (err, res) => {
-//     //...
-// });
+module.exports = {
+    generateUserID
+}
